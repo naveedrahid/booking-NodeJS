f3cb0442dc19cf3ebae7c8394d8593912d54ec54
naveedrahid <naveedrashid685@gmail.com>
2025-04-29 20:58:16 +0500

    test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,6 @@ const {getUsers, createUser} = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', getUsers);
-
 router.post('/', createUser);
 
 module.exports = router;
